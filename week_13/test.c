f16834f5df7f442025831d226b2b14b0d7544382
Ahyun-J <jah29741@gmail.com>
2022-05-27 06:03:43 +0900

    test2

--- week_13/test.c
+++ week_13/test.c
@@ -1,42 +1,60 @@
 #include <stdio.h>
+#include <string.h> //strlen()
 
-void bubble_sort(int arr[], int count)    // 매개변수로 정렬할 배열과 요소의 개수를 받음
-{
-    int temp;
-
-    for (int i = 0; i < count; i++)    // 요소의 개수만큼 반복
+int find_dic(char* x_dic, int x_dic_cnt,char x_ch){
+    for(int j=0; j<x_dic_cnt; j++)
     {
-        for (int j = 0; j < count - 1; j++)   // 요소의 개수 - 1만큼 반복
+        if(x_dic[j] == x_ch)
+            return j; //딕셔너리 내 위치 발견
+    }
+    return 0; //없음   
+}
+
+void stack_dic(char* x_dic, int* x_dic_cnt,char x_ch){
+    x_dic[*x_dic_cnt] = x_ch;
+    (*x_dic_cnt)=(*x_dic_cnt)+1;
+}
+
+void print_dic(char* x_dic, int x_dic_cnt){
+    for(int i=0; i<x_dic_cnt; i++)
+    {
+        printf("%c",x_dic[i]);               
+
+        if(i==x_dic_cnt-2)
         {
-            if (arr[j] > arr[j + 1])          // 현재 요소의 값과 다음 요소의 값을 비교하여
-            {                                 // 큰 값을
-                temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;            // 다음 요소로 보냄
-            }
+            printf("\n");
+            break;
         }
+        else
+            printf(",");                   
     }
 }
 
-int main()
-{int numArr[10];
-int arr_cnt=0;
-    printf("숫자 5개를 입력해주세요 ? "); // 6 8 5 2 9
-    for (int i = 0; i < 10; i++)
+void local_main(){
+    char str[1024];
+
+    while (1)
     {
-        scanf("%d", &numArr[i]);
-        arr_cnt = i + 1;
+        char dic[1024] = {'\0'}; //NULL
+        int dic_cnt = 0;   
+        dic_cnt = 0;
+        printf("공백 문자가 포함된 문자열을 입력하시오 ? ");
+        fgets(str, sizeof(str), stdin);
+        //Step1     매번문자열 입력이 번거롭다.
+        //sprintf(str,"%s","Hello World 1212");
+        //printf("%s %d \n",str,strlen(str));
+
+        //Step2
+        for (int i = 0; i < strlen(str); i++)
+        {
+            if (find_dic(dic, dic_cnt, str[i]) == 0)
+                stack_dic(dic, &dic_cnt, str[i]);
+        }
+
+        print_dic(dic, dic_cnt);
     }
+}
 
- 
-    bubble_sort(numArr, sizeof(numArr) / sizeof(int));    // 거품 정렬 함수 호출
-
-    for (int i = 0; i < 10; i++)
-    {
-        printf("%d ", numArr[i]);    // 1 2 3 4 5 6 7 8 9 10
-    }
-
-    printf("\n");
-
-    return 0;
+int main() {
+    local_main(); //전역변수 x
 }
